537b00a950ee12d1be1c061dbefbe1e519fa6f0e
k.hamada <k.hamada.1988@gmail.com>
2018-09-30 22:49:18 +0900

    Add /year/month/day

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,30 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate premium_friday;
+
+use premium_friday::*;
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello from Rocket"
+fn index() -> &'static str {
+    "
+    USAGE
+      GET /<year>/<month>/<day>
+    "
+}
+
+#[get("/<year>/<month>/<day>")]
+fn ask(year: i32, month: u32, day: u32) -> Option<String> {
+    let p = PremiumFriday::new().set_start_date(2017, 2, 24);
+    p.is_premium_friday(year, month, day).map(|result| format!("{}", result))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![
+            index,
+            ask,
+            today
+        ])
+        .launch();
 }
