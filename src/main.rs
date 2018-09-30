ff263776346d1309e3c3b212b2696b168284b214
k.hamada <k.hamada.1988@gmail.com>
2018-09-30 23:04:31 +0900

    Add /today

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate chrono;
 extern crate premium_friday;
 
+use rocket::response::Redirect;
+use chrono::prelude::*;
 use premium_friday::*;
 
 #[get("/")]
@@ -11,6 +14,7 @@ fn index() -> &'static str {
     "
     USAGE
       GET /<year>/<month>/<day>
+      GET /today
     "
 }
 
@@ -18,6 +22,14 @@ fn index() -> &'static str {
 fn ask(year: i32, month: u32, day: u32) -> Option<String> {
     let p = PremiumFriday::new().set_start_date(2017, 2, 24);
     p.is_premium_friday(year, month, day).map(|result| format!("{}", result))
+}
+
+#[get("/today")]
+fn today() -> Redirect  {
+    let utc_today = Utc::today().naive_utc();
+    let tz_offset = FixedOffset::east(9 * 3600);
+    let today = tz_offset.from_utc_date(&utc_today);
+    Redirect::to(format!("/{}/{}/{}", today.year(), today.month(), today.day()))
 }
 
 fn main() {
