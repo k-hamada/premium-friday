07d31080045ca5c061dbdfa8dc4c00717a548d20
k.hamada <k.hamada.1988@gmail.com>
2018-10-28 23:00:16 +0900

    New without Default derive

--- tests/premium-friday.rs
+++ tests/premium-friday.rs
@@ -4,26 +4,26 @@ use premium_friday::*;
 
 #[test]
 fn test_is_premium_friday() {
-    let p = PremiumFriday::new();
+    let p = PremiumFriday::default();
     assert!(p.is_premium_friday(2017, 2, 24).unwrap());
 }
 
 #[test]
 fn test_is_not_premium_friday() {
-    let p = PremiumFriday::new();
+    let p = PremiumFriday::default();
     assert!(!p.is_premium_friday(2017, 2, 23).unwrap());
     assert!(!p.is_premium_friday(2017, 2, 25).unwrap());
 }
 
 #[test]
 fn test_is_not_last_friday() {
-    let p = PremiumFriday::new();
+    let p = PremiumFriday::default();
     assert!(p.is_premium_friday(2017, 1, 27).unwrap());
     assert!(!p.is_premium_friday(2017, 3, 3).unwrap());
 }
 
 #[test]
 fn test_is_premium_friday_in_range() {
-    let p = PremiumFriday::new().set_start_date(2017, 2, 24);
+    let p = PremiumFriday::default().set_start_date(2017, 2, 24);
     assert!(!p.is_premium_friday(2017, 1, 27).unwrap());
 }
